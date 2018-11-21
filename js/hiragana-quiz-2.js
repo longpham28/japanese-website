fb7c3f35f44e6849a9cea2eb53cdea597433ed09
longpham28 <44454827+longpham28@users.noreply.github.com>
2018-11-21 11:06:54 +0900

    done hiragana-quiz

--- js/hiragana-quiz-2.js
+++ js/hiragana-quiz-2.js
@@ -19,124 +19,159 @@ let wrongAnswer1 = new quiz();
 let wrongAnswer2 = new quiz();
 let wrongAnswer3 = new quiz();
 
-correctAnswer.getCharacters();
-wrongAnswer1.getCharacters();
-wrongAnswer2.getCharacters();
-wrongAnswer3.getCharacters();
+let firstAnswer = "";
+let secondAnswer = "";
+let thirdAnswer = "";
+let fourthAnswer = "";
 
-while (1 > 0) {
-  if (correctAnswer == wrongAnswer1 || correctAnswer == wrongAnswer2 || correctAnswer == wrongAnswer3 || wrongAnswer1 == wrongAnswer2 || wrongAnswer1 == wrongAnswer3 || wrongAnswer2 == wrongAnswer3) {
+
+function getAnswers() {
+  let choices = [];
+
+  function answers() {
+    correctAnswer.getCharacters();
     wrongAnswer1.getCharacters();
     wrongAnswer2.getCharacters();
     wrongAnswer3.getCharacters();
-  } else break;
+
+    while (1 > 0) {
+      if (correctAnswer == wrongAnswer1 || correctAnswer == wrongAnswer2 || correctAnswer == wrongAnswer3 || wrongAnswer1 == wrongAnswer2 || wrongAnswer1 == wrongAnswer3 || wrongAnswer2 == wrongAnswer3) {
+        wrongAnswer1.getCharacters();
+        wrongAnswer2.getCharacters();
+        wrongAnswer3.getCharacters();
+      } else break;
+    }
+
+    choices = [correctAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3];
+  }
+
+  answers();
+
+  firstAnswer = choices[Math.floor(Math.random() * choices.length)].romaji;
+  secondAnswer = choices[Math.floor(Math.random() * choices.length)].romaji;
+  thirdAnswer = choices[Math.floor(Math.random() * choices.length)].romaji;
+  fourthAnswer = choices[Math.floor(Math.random() * choices.length)].romaji;
+
+  while (1 > 0) {
+    if (firstAnswer == secondAnswer || firstAnswer == thirdAnswer || firstAnswer == fourthAnswer || secondAnswer == thirdAnswer || secondAnswer == fourthAnswer || thirdAnswer == fourthAnswer) {
+      secondAnswer = choices[Math.floor(Math.random() * choices.length)].romaji;
+      thirdAnswer = choices[Math.floor(Math.random() * choices.length)].romaji;
+      fourthAnswer = choices[Math.floor(Math.random() * choices.length)].romaji;
+    } else break;
+  }
 }
-
-
-let choice = [correctAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3];
-
-let firstAnswer = choice[Math.floor(Math.random() * choice.length)].romaji;
-let secondAnswer = choice[Math.floor(Math.random() * choice.length)].romaji;
-let thirdAnswer = choice[Math.floor(Math.random() * choice.length)].romaji;
-let fourthAnswer = choice[Math.floor(Math.random() * choice.length)].romaji;
-
-while (1 > 0) {
-  if (firstAnswer == secondAnswer || firstAnswer == thirdAnswer || firstAnswer == fourthAnswer || secondAnswer == thirdAnswer || secondAnswer == fourthAnswer || thirdAnswer == fourthAnswer) {
-    secondAnswer = choice[Math.floor(Math.random() * choice.length)].romaji;
-    thirdAnswer = choice[Math.floor(Math.random() * choice.length)].romaji;
-    fourthAnswer = choice[Math.floor(Math.random() * choice.length)].romaji;
-  } else break;
-}
-
-console.log(firstAnswer);
 
 
 $(document).ready(() => {
-  $(".question").html("WHAT IS THE PRONOUNCIATION OF " + correctAnswer.hiragana);
+  const outPut = () => {
+    getAnswers();
+    $(".question").html("WHAT IS THE PRONOUNCIATION OF " + correctAnswer.hiragana);
 
-  $("#1st-answer").html(firstAnswer);
-  $("#2nd-answer").html(secondAnswer);
-  $("#3rd-answer").html(thirdAnswer);
-  $("#4th-answer").html(fourthAnswer);
+    $("#1st-answer").html(firstAnswer);
+    $("#2nd-answer").html(secondAnswer);
+    $("#3rd-answer").html(thirdAnswer);
+    $("#4th-answer").html(fourthAnswer);
 
-  if (firstAnswer == correctAnswer.romaji) {
-    $("#1st-answer").on('click', () => {
-      $(".answer").not("#1st-answer").html("");
-      $(".judgment").css({
-        "background-color": "blue",
-        "color": "white "
-      })
-      $(".judgment").html("CORRECT ANSWER!");
-      $(".next-previous").fadeIn();
-    });
-    $(".answer").not("#1st-answer").on('click', (event) => {
-      $(event.currentTarget).html("");
-      $(".judgment").css({
-        "color": "white",
-        "background-color": "red"
+    if (firstAnswer == correctAnswer.romaji) {
+      $("#1st-answer").on('click', () => {
+        $(".answer").addClass("deactive");
+        $(".answer").off('click');
+        $(".answer").not("#1st-answer").html("");
+        $(".judgment").css({
+          "background-color": "#222233",
+          "color": "white "
+        })
+        $(".judgment").html("CORRECT ANSWER!");
+        $(".judgment").show();
+        $(".next-question").fadeIn();
       });
-      $(".judgment").html("WRONG ANSWER!");
-    })
+      $(".answer").not("#1st-answer").on('click', (event) => {
+        $(event.currentTarget).html("");
+        $(".judgment").css({
+          "color": "white",
+          "background-color": "red"
+        });
+        $(".judgment").html("WRONG ANSWER!");
+        $(".judgment").show();
+      })
+    }
+
+    if (secondAnswer == correctAnswer.romaji) {
+      $("#2nd-answer").on('click', () => {
+        $(".answer").addClass("deactive");
+        $(".answer").off('click');
+        $(".answer").not("#2nd-answer").html("");
+        $(".judgment").css({
+          "background-color": "#222233",
+          "color": "white "
+        })
+        $(".judgment").html("CORRECT ANSWER!");
+        $(".judgment").show();
+        $(".next-question").fadeIn();
+      });
+      $(".answer").not("#2nd-answer").on('click', (event) => {
+        $(event.currentTarget).html("");
+        $(".judgment").css({
+          "color": "white",
+          "background-color": "red"
+        });
+        $(".judgment").html("WRONG ANSWER!");
+        $(".judgment").show();
+      });
+
+    }
+    if (thirdAnswer == correctAnswer.romaji) {
+      $("#3rd-answer").on('click', () => {
+        $(".answer").addClass("deactive");
+        $(".answer").off('click');
+        $(".answer").not("#3rd-answer").html("");
+        $(".judgment").css({
+          "background-color": "#222233",
+          "color": "white "
+        })
+        $(".judgment").html("CORRECT ANSWER!");
+        $(".judgment").show();
+        $(".next-question").fadeIn();
+      });
+      $(".answer").not("#3rd-answer").on('click', (event) => {
+        $(event.currentTarget).html("");
+        $(".judgment").css({
+          "color": "white",
+          "background-color": "red"
+        });
+        $(".judgment").html("WRONG ANSWER!");
+        $(".judgment").show();
+      });
+    }
+
+    if (fourthAnswer == correctAnswer.romaji) {
+      $("#4th-answer").on('click', () => {
+        $(".answer").addClass("deactive");
+        $(".answer").off('click');
+        $(".answer").not("#4th-answer").html("");
+        $(".judgment").css({
+          "background-color": "#222233",
+          "color": "white "
+        })
+        $(".judgment").html("CORRECT ANSWER!");
+        $(".judgment").show();
+        $(".next-question").fadeIn();
+      });
+      $(".answer").not("#4th-answer").on('click', (event) => {
+        $(event.currentTarget).html("");
+        $(".judgment").css({
+          "color": "white",
+          "background-color": "red"
+        });
+        $(".judgment").html("WRONG ANSWER!");
+        $(".judgment").show();
+      });
+    }
   }
 
-  if (secondAnswer == correctAnswer.romaji) {
-    $("#2nd-answer").on('click', () => {
-      $(".answer").not("#2nd-answer").html("");
-      $(".judgment").css({
-        "background-color": "blue",
-        "color": "white "
-      })
-      $(".judgment").html("CORRECT ANSWER!");
-      $(".next-previous").fadeIn();
-    });
-    $(".answer").not("#2nd-answer").on('click', (event) => {
-      $(event.currentTarget).html("");
-      $(".judgment").css({
-        "color": "white",
-        "background-color": "red"
-      });
-      $(".judgment").html("WRONG ANSWER!");
-    });
+  outPut();
 
-  }
-  if (thirdAnswer == correctAnswer.romaji) {
-    $("#3rd-answer").on('click', () => {
-      $(".answer").not("#3rd-answer").html("");
-      $(".judgment").css({
-        "background-color": "blue",
-        "color": "white "
-      })
-      $(".judgment").html("CORRECT ANSWER!");
-      $(".next-previous").fadeIn();
-    });
-    $(".answer").not("#3rd-answer").on('click', (event) => {
-      $(event.currentTarget).html("");
-      $(".judgment").css({
-        "color": "white",
-        "background-color": "red"
-      });
-      $(".judgment").html("WRONG ANSWER!");
-    });
-  }
-
-  if (fourthAnswer == correctAnswer.romaji) {
-    $("#4th-answer").on('click', () => {
-      $(".answer").not("#4th-answer").html("");
-      $(".judgment").css({
-        "background-color": "blue",
-        "color": "white "
-      })
-      $(".judgment").html("CORRECT ANSWER!");
-      $(".next-previous").fadeIn();
-    });
-    $(".answer").not("#4th-answer").on('click', (event) => {
-      $(event.currentTarget).html("");
-      $(".judgment").css({
-        "color": "white",
-        "background-color": "red"
-      });
-      $(".judgment").html("WRONG ANSWER!");
-    });
-  }
-
+  $(".next-question").on('click', () => {
+    location.reload()
+  });
 });
