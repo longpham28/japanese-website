6926b7b8e061ef4a66c14b599bf20778a7bd3059
longpham28 <44454827+longpham28@users.noreply.github.com>
2018-12-12 14:32:28 +0900

    done navbar

--- js/side-nav.js
+++ js/side-nav.js
@@ -1,3 +1,10 @@
+const activeNav = document.querySelectorAll('a');
+for (let i of activeNav) {
+  if (i.textContent === document.title) {
+    i.style.fontWeight = 'bold';
+    i.style.fontSize = '20px';
+  }
+}
 $(document).ready(() => {
   $(".open-button").on('click', () => {
     $(".side-nav").show();
